1c105fd61edafc098119cced7765700929136d55
DavideCriscuolo <davide.alcatel@gmail.com>
2025-06-10 15:42:48 +0200

    Aggiungi file HTML e JavaScript per la gestione delle squadre di calcio

---
+++ snack2/main.js
@@ -0,0 +1,69 @@
+// Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”. Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+// Creare un array di oggetti di squadre di calcio.
+// Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+const clubs = [
+  {
+    name: "Roma",
+    points: 0,
+    falliSub: 0,
+  },
+  {
+    name: "Inter",
+    points: 0,
+    falliSub: 0,
+  },
+  {
+    name: "Napoli",
+    points: 0,
+    falliSub: 0,
+  },
+  {
+    name: "Milan",
+    points: 0,
+    falliSub: 0,
+  },
+  {
+    name: "Juventus",
+    points: 0,
+    falliSub: 0,
+  },
+  {
+    name: "Torino",
+    points: 0,
+    falliSub: 0,
+  },
+  {
+    name: "Fiorentina",
+    points: 0,
+    falliSub: 0,
+  },
+  {
+    name: "Lazio",
+    points: 0,
+    falliSub: 0,
+  },
+  {
+    name: "Bologna",
+    points: 0,
+    falliSub: 0,
+  },
+  {
+    name: "Genova",
+    points: 0,
+    falliSub: 0,
+  },
+];
+
+// Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+for (let i = 0; i < clubs.length; i++) {
+  const club = clubs[i];
+  club.points = getRndInteger(0, 80);
+  club.falliSub = getRndInteger(0, 30);
+}
+console.log(clubs);
