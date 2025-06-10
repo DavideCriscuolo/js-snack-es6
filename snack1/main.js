aaa6edd9d52f826ebfd71193b9ed255bc2a2938f
DavideCriscuolo <davide.alcatel@gmail.com>
2025-06-10 13:58:09 +0200

    Aggiungi file HTML e JavaScript per la gestione delle bici da corsa

---
+++ snack1/main.js
@@ -0,0 +1,50 @@
+//Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore.
+
+
+
+const bikes = [
+  {
+    name: "mountain-bike",
+    peso: 20,
+  },
+  {
+    name: "cross-bike",
+    peso: 30,
+  },
+  {
+    name: "small-bike",
+    peso:10,
+  },
+  {
+    name: "big-bike",
+    peso: 30,
+  },
+  {
+    name: "kid-bike",
+    peso: 15,
+  },
+  {
+    name: "adult-bike",
+    peso: 30,
+  },
+  {
+    name: "bmx-bike",
+    peso: 25,
+  },
+  {
+    name: "mountain-bike",
+    peso: 35,
+  },
+  {
+    name: "mountain-bike",
+    peso: 40,
+  },
+  {
+    name: "mountain-bike",
+    peso: 37,
+  },
+];
+
+for (let i = 0;i<){
+    
+}
